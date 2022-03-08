3f9fdff4e245f073b7115e99917aaf414653b7ef
소울치킨 <soulfever01@naver.com>
2022-03-08 22:08:08 +0900

    [Python] 버튼을 클릭하면 응답하고 덧셈 퀴즈를 내는 창 만들기 구현

---
+++ PySide/ui실행하기.py
@@ -0,0 +1,21 @@
+import sys
+from PySide6.QtWidgets import QMainWindow, QApplication
+from ui import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow,self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication()
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
+
+# 디자이너 경로 : ./myvenv/lib/python3.10/site-packages/PySide6/Designer.app/Contents/MacOS/Designer
+# uci 경로 : ./myvenv/lib/python3.10/site-packages/PySide6/Qt/libexec/uic
+#  (uci 경로) -g python (해당파일명).ui > (만들경로/만들파일명).py
